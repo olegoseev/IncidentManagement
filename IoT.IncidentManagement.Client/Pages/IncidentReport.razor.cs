27505e9bc16eff9391f94a72af0364c6d0fea856
Oseev, Oleg <oleg.oseev@verizonwireless.com>
2021-11-01 21:14:36 -0700

    added report empty page

---
+++ IoT.IncidentManagement.Client/Pages/IncidentReport.razor.cs
@@ -0,0 +1,41 @@
+﻿using IoT.IncidentManagement.ClientDomain.Entities;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.IncidentManagement.Client.Pages
+{
+    public partial class IncidentReport
+    {
+        #region Services
+        [Inject] private IMediator Mediator { get; set; }
+        #endregion
+
+        #region Parameters
+        [Parameter] public int IncidentId { get; set; }
+        #endregion
+
+
+        #region Private fields
+        private Incident incident;
+        private Note note;
+        private Participant participant;
+        private ClosureAction closureAction;
+        #endregion
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+
+
+
+    }
+}
